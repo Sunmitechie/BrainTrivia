539c856ce4a36f8b77abab5a291c2a244687538d
Sunmibare Idowu <sunmijewel@gmail.com>
2025-01-30 00:18:24 +0100

    Create questions.py

---
+++ questions/questions.py
@@ -0,0 +1,17 @@
+[
+    {
+        "text": "What is the capital of France?",
+        "options": ["A) Madrid", "B) Berlin", "C) Paris", "D) Rome"],
+        "answer": "C"
+    },
+    {
+        "text": "What is 7 + 8?",
+        "options": ["A) 13", "B) 15", "C) 14", "D) 16"],
+        "answer": "B"
+    },
+    {
+        "text": "Which planet is known as the Red Planet?",
+        "options": ["A) Venus", "B) Mars", "C) Jupiter", "D) Saturn"],
+        "answer": "B"
+    }
+]
